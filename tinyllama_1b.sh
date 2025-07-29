e2cc67d2d9e7f872ab717dcbe22c01623a931360
Zhoutong-Wu <154485539+Zhoutong-Wu@users.noreply.github.com>
2025-07-29 14:34:50 +0800

    Add files via upload

---
+++ tinyllama_1b.sh
@@ -0,0 +1,22 @@
+# LLaMA-1B, GaLore-Adam, 8 A100, 1 Node
+lr=0.001
+wd=0.1
+name="tinyllama1b-base-adamw-lr${lr}-wd${wd}"
+torchrun --standalone --nproc_per_node 2 torchrun_main_skip_tinyllama.py\
+    --model_config configs/tinyllama_1b.json \
+    --lr $lr \
+    --rank 1024 \
+    --update_proj_gap 200 \
+    --batch_size 16 \
+    --total_batch_size 512 \
+    --num_training_steps 25000 \
+    --warmup_steps 2500 \
+    --weight_decay $wd \
+    --dtype bfloat16 \
+    --eval_every 1000 \
+    --save_every 2000 \
+    --name $name \
+    --save_dir checkpoints/TinyLLaMA-skip-1B \
+    --grad_clipping 1.0 \
+    --max_length 1024 \
+    --use_hf_model 
